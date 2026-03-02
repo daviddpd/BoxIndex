018023afbf2984c490cfe8cb97f6e5012ffc9959
David P. Discher <dpd@dpdtech.com>
2026-03-02 13:19:24 -0800

    Initial Commit

---
+++ BoxIndexTests/BoxIndexTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoxIndexTests.swift
+//  BoxIndexTests
+//
+//  Created by David P. Discher on 3/2/26.
+//
+
+import Testing
+@testable import BoxIndex
+
+struct BoxIndexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
